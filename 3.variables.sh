430dad60870ebd5cce2274368949a9aeeb6a8380
chinna <chinnamachireddy222@gmail.com>
2024-05-12 12:32:44 +0530

    updated

--- 3.variables.sh
+++ 3.variables.sh
@@ -1,6 +1,6 @@
 PERSON1=Ramesh
 PERSON2=Suresh
-echo "PERSON1:: helo suresh GM"
-echo "PERSON2:: hello ramesh GM how are you "
-echo "PERSON2 :: i am good how are you ramesh"
-echo "PERSON 1:: i am fine thanks"
+echo "$PERSON1:: helo $PERSON2 GM"
+echo "$PERSON2:: hello $PERSON2 GM how are you "
+echo "$PERSON2 :: i am good how are you $PEROSN1"
+echo "$PERSON 1:: i am fine thanks"
