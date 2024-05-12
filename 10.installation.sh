4993091ebcdb3c1fae34ab0b568303fe89dc3268
chinna <chinnamachireddy222@gmail.com>
2024-05-12 16:27:04 +0530

    updated

---
+++ 10.installation.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+USERID=$(id -u)
+if [$USERID -ne 0]
+then
+    echo "Plese run the script with root access"
+    exit 1 # manually exit if error comes.
+else
+    echo "you are the super user"
+fi
+
+dnf install mysql -y
