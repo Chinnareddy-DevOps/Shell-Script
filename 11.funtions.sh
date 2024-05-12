d1709f229cdcfcc6a53fed2373ff5e8747f4112c
chinna <chinnamachireddy222@gmail.com>
2024-05-12 19:18:30 +0530

    updated

---
+++ 11.funtions.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+VALIDATE(){
+     if [ $1 -ne 0 ]
+   then
+    echo "$2...FAILURE"
+        exit 1
+    else
+        echo "$2...SUCCESS"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "Plese run the script with root access"
+    exit 1 # manually exit if error comes.
+else
+    echo "you are the super user"
+fi
+
+dnf install mysql -y
+
+VALIDATE $? "installing mysql"
+
+
+dnf install git -y
+
+VALIDATE $? "installing git"
+
