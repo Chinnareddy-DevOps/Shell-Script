5485650d9cb614ce871909261ff85a74047f6497
chinna <chinnamachireddy222@gmail.com>
2024-05-12 14:49:42 +0530

    updated

---
+++ 8.special-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "All variables: $@"
+echo "number of variables passed: $#"
+echo "Script Name : $0"
+echo "current working directory: $PWD"
+echo "home directory of current user: $HOME"
+echo "which user is running this script: $USER"
