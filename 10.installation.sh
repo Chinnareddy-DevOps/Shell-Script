78677c553298698757ee24523f237dab8f8a4ba7
chinna <chinnamachireddy222@gmail.com>
2024-05-12 16:45:50 +0530

    updated

--- 10.installation.sh
+++ 10.installation.sh
@@ -10,3 +10,19 @@ else
 fi
 
 dnf install mysql -y
+
+if [ $? -ne 0 ]
+then
+    echo "Mysql Installation Faillure"
+else
+    echo "My sql installation successfull"
+fi
+
+dnf install git -y
+
+if [ $? -ne 0 ]
+then
+    echo "INSTALLATION OF git failure"
+else
+    echo "Instalation of Git successfull"
+fi
